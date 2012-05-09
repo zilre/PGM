8526353288fb8f7cdae8b89deeda4fa5e67ed624
indapa <indapa@gmail.com>
2012-05-08 21:22:52 -0400

    finsihed implementation

--- Prog5/CheckConvergence.m
+++ Prog5/CheckConvergence.m
@@ -14,18 +14,25 @@
 %
 % Copyright (C) Daphne Koller, Stanford University, 2012
 
+%call the function as CheckConvergence(exampleINPUT.t3a1{1}, exampleINPUT.t3a1{2}) with the test
+%input
+
 function converged = CheckConvergence(mNew, mOld);
 converged = true;
 thresh = 1.0e-6;
 %converged should be 1 if converged, 0 otherwise.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% YOUR CODE HERE
-% 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[nrow ncol] = size(mNew);
+
+for i=1:nrow
+    for j=1:ncol
+        if sum( abs(mNew(i,j).val-mOld(i,j).val) > thresh ) ~=0
+            converged=false;
+        end
+        
+    end
+end
 
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 return;
